7b3cf6f91d2c21cf7e9072b848b1bbf333723ac0
kerouace <43724698+Kerouace@users.noreply.github.com>
2023-09-28 13:37:36 +0200

    add todo-comments plugin and spelling

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,30 @@
+-- set signs for various uses
+Error_sign = " "
+Warn_sign = " "
+Info_sign = " "
+Hint_sign = " "
+Hack_sign = " "
+Todo_sign = " "
+Perfect_sign = " "
+Test_sign = " "
+
+return{
+  -- todo symbols and highlighting
+  'folke/todo-comments.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    keywords = {
+      ERRO = { icon = Error_sign, color = "error" },
+      WARN = { icon = Warn_sign, color = "warning" },
+      HACK = { icon = Hack_sign, color = "warning" },
+      HINT = { icon = Hint_sign, color = "hint" },
+      TODO = { icon = Todo_sign, color = "info" },
+      INFO = { icon = Info_sign, color = "hint", alt = { "NOTE" } },
+      PERF = { icon = Perfect_sign, color = "default" },
+      TEST = { icon = Test_sign, color = "test" }
+    },
+    colors = {
+      default = { "Operator" }
+    }
+  }
+}
