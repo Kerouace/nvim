bb84558459ecf1279c592eaa54d77ab71ae0543e
kerouace <43724698+Kerouace@users.noreply.github.com>
2024-02-28 18:23:34 +0100

    add some convenients settings to vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,7 +4,12 @@ return {
     'lervag/vimtex',
     lazy = false,
     init = function()
-      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_view_method = 'zathura';
+      --vim.g.vimtex_quickfix_ignore_filters = {""};
+      --vim.g.vimtex_quickfix_method = 'pplatex';
+      vim.g.vimtex_quickfix_mode = 2;
+      vim.g.vimtex_quickfix_autoclose_after_keystrokes = 20;
+      vim.g.vimtex_quickfix_open_on_warning = 1;
     end
   }
 }
