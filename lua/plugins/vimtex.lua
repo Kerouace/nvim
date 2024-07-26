6d1a727a34567cb66f6d60188289db5019d27608
kerouace <43724698+Kerouace@users.noreply.github.com>
2024-07-26 14:50:31 +0200

    add update of vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,11 +5,13 @@ return {
     lazy = false,
     init = function()
       vim.g.vimtex_view_method = 'zathura';
-      vim.g.vimtex_compiler_latexmk_engines = 'luatex';
+      vim.g.vimtex_compiler_enable = 1;
+      vim.g.vimtex_compiler_method = 'latexmk';
       --vim.g.vimtex_quickfix_ignore_filters = {""};
       --vim.g.vimtex_quickfix_method = 'pplatex';
       vim.g.vimtex_quickfix_mode = 2;
       vim.g.vimtex_quickfix_autoclose_after_keystrokes = 10;
+      vim.g.vimtex_quickfix_open_on_warning = 0;
       vim.g.vimtex_quickfix_open_on_warning = 0;
     end
   }
