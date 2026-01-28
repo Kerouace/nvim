10de88e32eec6a04710fc8ce07dce8032e332a1b
Kerouace <43724698+Kerouace@users.noreply.github.com>
2026-01-28 09:51:40 +0100

    add gpl snippet

--- luasnippets/all.lua
+++ luasnippets/all.lua
@@ -10,6 +10,48 @@ local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
 local rep = require("luasnip.extras").rep
 
+-- Comment styles per filetype
+local comment_chars = {
+  lua = "--",
+  python = "#",
+  verilog = "//",
+  systemverilog = "//",
+  vhdl = "--",
+  c = "//",
+  cpp = "//",
+  make = "#",
+}
+
+local function get_comment_char()
+  return comment_chars[vim.bo.filetype] or "#"
+end
+
+local function gpl_header()
+  local c = get_comment_char()
+  local year = os.date("%Y")
+  return sn(nil, {
+    t(c .. " Copyright (C) " .. year .. " "),
+    i(1, "Your Name"),
+    t({
+      "",
+      c,
+      c .. " This program is free software: you can redistribute it and/or modify",
+      c .. " it under the terms of the GNU General Public License as published by",
+      c .. " the Free Software Foundation, either version 3 of the License, or",
+      c .. " (at your option) any later version.",
+      c,
+      c .. " This program is distributed in the hope that it will be useful,",
+      c .. " but WITHOUT ANY WARRANTY; without even the implied warranty of",
+      c .. " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the",
+      c .. " GNU General Public License for more details.",
+      c,
+      c .. " You should have received a copy of the GNU General Public License",
+      c .. " along with this program. If not, see <https://www.gnu.org/licenses/>.",
+      "",
+    }),
+  })
+end
+
 return {
   s(
     { -- Table 1: snippet parameters
@@ -24,5 +66,14 @@ return {
           return os.date "%Y-%m-%d"
       end),
     }
+  ),
+
+  s(
+    {
+      trig = "gpl",
+      dscr = "GPL-3.0-or-later copyright header",
+      snippetType = "snippet",
+    },
+    { d(1, gpl_header) }
   ),
 }
