4602805454a1909dcd983de3a2feee453e985281
Kerouace <43724698+Kerouace@users.noreply.github.com>
2026-03-10 10:22:48 +0100

    add keybinding showing details in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -12,7 +12,18 @@ return {
         show_hidden = true,
       },
       keymaps = {
-        ["~"] = false
+        ["~"] = false,
+        ["gd"] = {
+              desc = "Toggle file detail view",
+              callback = function()
+                detail = not detail
+                if detail then
+                  require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+                else
+                  require("oil").set_columns({ "icon" })
+                end
+              end,
+            },
       },
     })
   end
