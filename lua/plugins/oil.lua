f463b6690528236fdfffd3a1e98f336c7c47f140
Kerouace <43724698+Kerouace@users.noreply.github.com>
2026-03-31 10:12:00 +0200

    add oil-status

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,30 +1,45 @@
 return {
-  "stevearc/oil.nvim",
-  config = function()
-    require('oil').setup({
-      columns = {
-        "icon",
-        -- "permissions",
-        -- "size",
-        -- "mtime",
-      },
-      view_options = {
-        show_hidden = true,
-      },
-      keymaps = {
-        ["~"] = false,
-        ["gd"] = {
-              desc = "Toggle file detail view",
-              callback = function()
-                detail = not detail
-                if detail then
-                  require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
-                else
-                  require("oil").set_columns({ "icon" })
-                end
-              end,
-            },
-      },
-    })
-  end
+  {
+    "stevearc/oil.nvim",
+    config = function()
+      require('oil').setup({
+        win_options = {
+          signcolumn = "yes:2",
+        },
+        columns = {
+          "icon",
+          -- "permissions",
+          -- "size",
+          -- "mtime",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+        keymaps = {
+          ["~"] = false,
+          ["gd"] = {
+                desc = "Toggle file detail view",
+                callback = function()
+                  detail = not detail
+                  if detail then
+                    require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+                  else
+                    require("oil").set_columns({ "icon" })
+                  end
+                end,
+              },
+        },
+      })
+    end
+  },
+
+  {
+  "refractalize/oil-git-status.nvim",
+
+  dependencies = {
+    "stevearc/oil.nvim",
+  },
+  config = true,
+},
+
 }
